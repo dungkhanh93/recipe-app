3321e414ca491a7252c9b9f12bd1df16b9f6388d
DungPV <dungpv@bravebits.vn>
2020-08-19 10:09:33 +0700

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// Global app controller
+import num from './test';
+
+console.log(`Day la content cua index.js`);
+console.log(`Day la content tu file test.js ${num}`);
---
+++ src/js/test.js
@@ -0,0 +1,6 @@
+console.log(`Day la content cua test.js`);
+export default 30;
+
+const x = 30;
+
+console.log(`Day la x ${x}`);
